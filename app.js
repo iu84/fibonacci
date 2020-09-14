fedd41e2e81da24d8b926820fff07c5c31ff8123
samurai <xcybza155@gmail.com>
2020-09-14 16:55:34 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+ function fib(n) {
+     if (n === 0) {
+         return 0;        
+     } else if (n === 1){
+         return 1;
+     }
+     return fib(n -1) + fib(n - 2);
+ }
+ const length = 40;
+ for (let i = 0; i <= length; i++) {
+     console.log(fib(i));
+ }
+
+// function fib(n) {
+    // console.log( n + "のフィッボナッチ数はxxxです" );
+// }
+// for (let i = 1; i <= 40; i++) {
+//  fib(i);
+// }
